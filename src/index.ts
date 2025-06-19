4cfa5b700b67ae4f3799db5f885aeb466e9bd6fc
bogdan <bogdan@MacBook-Pro-bogdan.local>
2025-06-19 18:56:04 +0300

    feat: add error handler

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-import express, { Request, Response } from 'express';
-import { Sequelize } from 'sequelize';
+import express, { NextFunction, Request, Response } from 'express';
 import { Umzug, SequelizeStorage } from 'umzug';
+import { updateBalance } from './user/user_controller';
+import { ApiError } from './common/error';
+import { User } from './user/model';
+import { Sequelize } from 'sequelize-typescript';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const sequelize = new Sequelize('postgres://postgres_user:postgres_password@localhost:5432/test');
+export const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres_user',
+  password: 'postgres_password',
+  database: 'test',
+  models: [User],
+});
 
 const umzug = new Umzug({
   migrations: { glob: 'migrations/*.js' },
@@ -23,6 +34,16 @@ app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('init express');
+});
+
+app.patch('/users/:id/balance', updateBalance);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof ApiError) {
+    res.status(err.statusCode).json({ message: err.message });
+  } else {
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
 app.listen(PORT, () => {
